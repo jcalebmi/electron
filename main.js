905d2b34487d102fe37088427cc3351f6f6f8d77
Caleb Iuliano <calebiuliano@gmail.com>
2021-07-09 17:16:43 -0400

    created electron app

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, webContents, BrowserWindow } = require('electron')
 const path = require('path')
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    height: 600
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://www.duckduckgo.com')
+  let position = mainWindow.getPosition();
+  position[0] += 200;
+  mainWindow.webContents.on('will-navigate', function (e, url) {
+    e.preventDefault()
+    if (!url.includes('https://duckduckgo.com/?')){
+      const child = new BrowserWindow({
+        width:800,
+        height: 600,
+        x: position[0] + 20,
+        y: position[1] + 20,
+        parent: mainWindow
+      })
+      child.loadURL(url);
+      position = child.getPosition();
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    } else {
+      mainWindow.loadURL(url);
+    }
+  })
 }
 
 // This method will be called when Electron has finished
@@ -24,7 +36,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -41,3 +53,4 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
